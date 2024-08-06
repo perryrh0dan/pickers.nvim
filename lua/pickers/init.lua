b422608c38bce43aea619062fb1ecf7b3441aab8
thomas <thomas@tpoe.dev>
2024-08-06 21:26:08 +0200

    feat(diff): make async and improve result list and preview

--- lua/pickers/init.lua
+++ lua/pickers/init.lua
@@ -1,6 +1,12 @@
 local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
+local previewers = require('telescope.previewers')
 local conf = require("telescope.config").values
+local utils = require("telescope.utils")
+local entry_display = require('telescope.pickers.entry_display')
+
+local strings = require "plenary.strings"
+local Job = require('plenary.job')
 
 local get_default_branch = "git rev-parse --symbolic-full-name refs/remotes/origin/HEAD | sed 's!.*/!!'"
 local base_branch = vim.fn.system(get_default_branch) or "main"
@@ -8,33 +14,113 @@ local base_branch = vim.fn.system(get_default_branch) or "main"
 local M = {}
 
 M.diff = function()
-    local command = "git diff --name-only $(git merge-base HEAD " .. base_branch .. " )"
+    local output_lines = {}
 
-    local handle = io.popen(command)
-    local output = handle:read("*a")
-    handle:close()
+    local job = Job:new({
+        command = "git",
+        args = { "diff", "--name-only", "master" },
+        on_stdout = function(_, line)
+            table.insert(output_lines, line)
+        end,
+        on_stderr = function(a, b)
+            vim.print(a)
+            vim.print(b)
+        end,
+        on_exit = function(_, exit_code, _)
+            if exit_code == 0 then
+                vim.schedule(function()
+                    M.openPicker(output_lines)
+                end)
+            else
+                print('Job failed with exit code:', exit_code)
+            end
+        end,
+    })
 
-    local list = {}
-    for token in string.gmatch(output, "[^%c]+") do
-        table.insert(list, token)
+    job:start()
+end
+
+M.openPicker = function(filePaths)
+    local icon, _ = utils.get_devicons("fname")
+    local icon_width = strings.strdisplaywidth(icon)
+
+    local displayer = entry_display.create {
+        separator = " ",
+        items = {
+            { width = icon_width },
+            { remaining = true },
+        },
+    }
+
+    local make_display = function(entry)
+        local display_path, path_style = utils.transform_path({
+            path_display = {
+                filename_first = {
+                    reverse_directories = false,
+                }
+            }
+        }, entry.path)
+        local icon, hl_group = utils.get_devicons(entry.path)
+
+        return displayer({
+            { icon, hl_group },
+            {
+                display_path,
+                function()
+                    return path_style
+                end
+                ,
+            },
+        })
     end
 
     local entry_maker = function(entry)
         return {
             value = entry,
-            display = entry,
+            display = make_display,
             ordinal = entry,
             path = entry,
         }
     end
 
+    local git_diff_previewer = previewers.new({
+        title = "Git Diff",
+        preview_fn = function(self, entry, status)
+            local filepath = entry.value
+            local buf = status.layout.preview.bufnr
+            vim.api.nvim_buf_set_option(buf, 'filetype', 'diff')
+
+            local output_lines = {}
+
+            Job:new({
+                command = 'git',
+                args = { 'diff', 'master', '--', filepath },
+                cwd = vim.loop.cwd(),
+                on_stdout = function(_, line)
+                    table.insert(output_lines, line)
+                    -- Schedule buffer update on the main thread
+                end,
+                on_stderr = function(a, data)
+                    if data then
+                        vim.print("stderr: " .. data) -- Debug stderr output
+                    end
+                end,
+                on_exit = function()
+                    vim.schedule(function()
+                        vim.api.nvim_buf_set_lines(buf, 0, -1, false, output_lines)
+                    end)
+                end
+            }):start()
+        end,
+    })
+
     pickers.new({}, {
         prompt_title = "Git diff between HEAD and " .. string.gsub(base_branch, '\n', ''),
         finder = finders.new_table {
-            results = list,
+            results = filePaths,
             entry_maker = entry_maker,
         },
-        previewer = conf.file_previewer {},
+        previewer = git_diff_previewer,
         sorter = conf.generic_sorter(opts),
     }):find()
 end
